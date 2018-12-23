8b6b7fbeff5a5644a2269a7528901c9948acbd56
burcu <burcu@myca.local>
2018-12-23 19:35:38 +0300

    New week

---
+++ Projects/ESGApp/target/scala-2.12/routes/main/controllers/javascript/JavaScriptReverseRoutes.scala
@@ -0,0 +1,257 @@
+// @GENERATOR:play-routes-compiler
+// @SOURCE:/home/burcu/git/Web-Application-for-Event-Sequence-Graph-Engine/Projects/ESGApp/conf/routes
+// @DATE:Sun Dec 23 18:46:55 EET 2018
+
+import play.api.routing.JavaScriptReverseRoute
+
+
+import _root_.controllers.Assets.Asset
+import _root_.play.libs.F
+
+// @LINE:6
+package controllers.javascript {
+
+  // @LINE:12
+  class ReversePersonController(_prefix: => String) {
+
+    def _defaultPrefix: String = {
+      if (_prefix.endsWith("/")) "" else "/"
+    }
+
+  
+    // @LINE:15
+    def sayHelloWithBodyParser: JavaScriptReverseRoute = JavaScriptReverseRoute(
+      "controllers.PersonController.sayHelloWithBodyParser",
+      """
+        function() {
+        
+          if (true) {
+            return _wA({method:"POST", url:"""" + _prefix + { _defaultPrefix } + """" + "sayHelloWithBodyParser"})
+          }
+        
+        }
+      """
+    )
+  
+    // @LINE:14
+    def sayHelloWithTestPlain: JavaScriptReverseRoute = JavaScriptReverseRoute(
+      "controllers.PersonController.sayHelloWithTestPlain",
+      """
+        function() {
+          return _wA({method:"POST", url:"""" + _prefix + { _defaultPrefix } + """" + "sayHelloWithTextPlain"})
+        }
+      """
+    )
+  
+    // @LINE:12
+    def sayHello: JavaScriptReverseRoute = JavaScriptReverseRoute(
+      "controllers.PersonController.sayHello",
+      """
+        function() {
+        
+          if (true) {
+            return _wA({method:"POST", url:"""" + _prefix + { _defaultPrefix } + """" + "sayHello"})
+          }
+        
+        }
+      """
+    )
+  
+  }
+
+  // @LINE:34
+  class ReverseAssets(_prefix: => String) {
+
+    def _defaultPrefix: String = {
+      if (_prefix.endsWith("/")) "" else "/"
+    }
+
+  
+    // @LINE:34
+    def versioned: JavaScriptReverseRoute = JavaScriptReverseRoute(
+      "controllers.Assets.versioned",
+      """
+        function(file1) {
+          return _wA({method:"GET", url:"""" + _prefix + { _defaultPrefix } + """" + "assets/" + (""" + implicitly[play.api.mvc.PathBindable[Asset]].javascriptUnbind + """)("file", file1)})
+        }
+      """
+    )
+  
+  }
+
+  // @LINE:8
+  class ReverseCountController(_prefix: => String) {
+
+    def _defaultPrefix: String = {
+      if (_prefix.endsWith("/")) "" else "/"
+    }
+
+  
+    // @LINE:8
+    def count: JavaScriptReverseRoute = JavaScriptReverseRoute(
+      "controllers.CountController.count",
+      """
+        function() {
+          return _wA({method:"GET", url:"""" + _prefix + { _defaultPrefix } + """" + "count"})
+        }
+      """
+    )
+  
+  }
+
+  // @LINE:25
+  class ReverseBooksController(_prefix: => String) {
+
+    def _defaultPrefix: String = {
+      if (_prefix.endsWith("/")) "" else "/"
+    }
+
+  
+    // @LINE:28
+    def edit: JavaScriptReverseRoute = JavaScriptReverseRoute(
+      "controllers.BooksController.edit",
+      """
+        function(id0) {
+          return _wA({method:"GET", url:"""" + _prefix + { _defaultPrefix } + """" + "books/edit/" + encodeURIComponent((""" + implicitly[play.api.mvc.PathBindable[Integer]].javascriptUnbind + """)("id", id0))})
+        }
+      """
+    )
+  
+    // @LINE:26
+    def create: JavaScriptReverseRoute = JavaScriptReverseRoute(
+      "controllers.BooksController.create",
+      """
+        function() {
+          return _wA({method:"GET", url:"""" + _prefix + { _defaultPrefix } + """" + "books/create"})
+        }
+      """
+    )
+  
+    // @LINE:27
+    def show: JavaScriptReverseRoute = JavaScriptReverseRoute(
+      "controllers.BooksController.show",
+      """
+        function(id0) {
+          return _wA({method:"GET", url:"""" + _prefix + { _defaultPrefix } + """" + "books/" + encodeURIComponent((""" + implicitly[play.api.mvc.PathBindable[Integer]].javascriptUnbind + """)("id", id0))})
+        }
+      """
+    )
+  
+    // @LINE:31
+    def destroy: JavaScriptReverseRoute = JavaScriptReverseRoute(
+      "controllers.BooksController.destroy",
+      """
+        function(id0) {
+          return _wA({method:"GET", url:"""" + _prefix + { _defaultPrefix } + """" + "books/delete/" + encodeURIComponent((""" + implicitly[play.api.mvc.PathBindable[Integer]].javascriptUnbind + """)("id", id0))})
+        }
+      """
+    )
+  
+    // @LINE:30
+    def save: JavaScriptReverseRoute = JavaScriptReverseRoute(
+      "controllers.BooksController.save",
+      """
+        function() {
+          return _wA({method:"POST", url:"""" + _prefix + { _defaultPrefix } + """" + "books/create"})
+        }
+      """
+    )
+  
+    // @LINE:29
+    def update: JavaScriptReverseRoute = JavaScriptReverseRoute(
+      "controllers.BooksController.update",
+      """
+        function() {
+          return _wA({method:"POST", url:"""" + _prefix + { _defaultPrefix } + """" + "books/edit"})
+        }
+      """
+    )
+  
+    // @LINE:25
+    def index: JavaScriptReverseRoute = JavaScriptReverseRoute(
+      "controllers.BooksController.index",
+      """
+        function() {
+          return _wA({method:"GET", url:"""" + _prefix + { _defaultPrefix } + """" + "books"})
+        }
+      """
+    )
+  
+  }
+
+  // @LINE:6
+  class ReverseHomeController(_prefix: => String) {
+
+    def _defaultPrefix: String = {
+      if (_prefix.endsWith("/")) "" else "/"
+    }
+
+  
+    // @LINE:6
+    def index: JavaScriptReverseRoute = JavaScriptReverseRoute(
+      "controllers.HomeController.index",
+      """
+        function() {
+          return _wA({method:"GET", url:"""" + _prefix + """"})
+        }
+      """
+    )
+  
+    // @LINE:17
+    def welcomeName: JavaScriptReverseRoute = JavaScriptReverseRoute(
+      "controllers.HomeController.welcomeName",
+      """
+        function(name0,lastName1) {
+          return _wA({method:"GET", url:"""" + _prefix + { _defaultPrefix } + """" + "welcome/" + encodeURIComponent((""" + implicitly[play.api.mvc.PathBindable[String]].javascriptUnbind + """)("name", name0)) + "/" + encodeURIComponent((""" + implicitly[play.api.mvc.PathBindable[String]].javascriptUnbind + """)("lastName", lastName1))})
+        }
+      """
+    )
+  
+  }
+
+  // @LINE:10
+  class ReverseAsyncController(_prefix: => String) {
+
+    def _defaultPrefix: String = {
+      if (_prefix.endsWith("/")) "" else "/"
+    }
+
+  
+    // @LINE:10
+    def message: JavaScriptReverseRoute = JavaScriptReverseRoute(
+      "controllers.AsyncController.message",
+      """
+        function() {
+          return _wA({method:"GET", url:"""" + _prefix + { _defaultPrefix } + """" + "message"})
+        }
+      """
+    )
+  
+  }
+
+  // @LINE:19
+  class ReverseESGController(_prefix: => String) {
+
+    def _defaultPrefix: String = {
+      if (_prefix.endsWith("/")) "" else "/"
+    }
+
+  
+    // @LINE:19
+    def saveESGToDB: JavaScriptReverseRoute = JavaScriptReverseRoute(
+      "controllers.ESGController.saveESGToDB",
+      """
+        function() {
+        
+          if (true) {
+            return _wA({method:"POST", url:"""" + _prefix + { _defaultPrefix } + """" + "saveESGToDB"})
+          }
+        
+        }
+      """
+    )
+  
+  }
+
+
+}
---
+++ Projects/ESGApp/target/scala-2.12/routes/main/controllers/routes.java
@@ -0,0 +1,30 @@
+// @GENERATOR:play-routes-compiler
+// @SOURCE:/home/burcu/git/Web-Application-for-Event-Sequence-Graph-Engine/Projects/ESGApp/conf/routes
+// @DATE:Sun Dec 23 18:46:55 EET 2018
+
+package controllers;
+
+import router.RoutesPrefix;
+
+public class routes {
+  
+  public static final controllers.ReversePersonController PersonController = new controllers.ReversePersonController(RoutesPrefix.byNamePrefix());
+  public static final controllers.ReverseAssets Assets = new controllers.ReverseAssets(RoutesPrefix.byNamePrefix());
+  public static final controllers.ReverseCountController CountController = new controllers.ReverseCountController(RoutesPrefix.byNamePrefix());
+  public static final controllers.ReverseBooksController BooksController = new controllers.ReverseBooksController(RoutesPrefix.byNamePrefix());
+  public static final controllers.ReverseHomeController HomeController = new controllers.ReverseHomeController(RoutesPrefix.byNamePrefix());
+  public static final controllers.ReverseAsyncController AsyncController = new controllers.ReverseAsyncController(RoutesPrefix.byNamePrefix());
+  public static final controllers.ReverseESGController ESGController = new controllers.ReverseESGController(RoutesPrefix.byNamePrefix());
+
+  public static class javascript {
+    
+    public static final controllers.javascript.ReversePersonController PersonController = new controllers.javascript.ReversePersonController(RoutesPrefix.byNamePrefix());
+    public static final controllers.javascript.ReverseAssets Assets = new controllers.javascript.ReverseAssets(RoutesPrefix.byNamePrefix());
+    public static final controllers.javascript.ReverseCountController CountController = new controllers.javascript.ReverseCountController(RoutesPrefix.byNamePrefix());
+    public static final controllers.javascript.ReverseBooksController BooksController = new controllers.javascript.ReverseBooksController(RoutesPrefix.byNamePrefix());
+    public static final controllers.javascript.ReverseHomeController HomeController = new controllers.javascript.ReverseHomeController(RoutesPrefix.byNamePrefix());
+    public static final controllers.javascript.ReverseAsyncController AsyncController = new controllers.javascript.ReverseAsyncController(RoutesPrefix.byNamePrefix());
+    public static final controllers.javascript.ReverseESGController ESGController = new controllers.javascript.ReverseESGController(RoutesPrefix.byNamePrefix());
+  }
+
+}
