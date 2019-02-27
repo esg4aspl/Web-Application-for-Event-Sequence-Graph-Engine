5125a77153b9c3e587bd94b1705c5199a26e4427
Burcu Erdogan <burcerdgn@gmail.com>
2019-02-27 20:36:54 +0300

    Update Module.java

--- Projects/ESGApp/app/Module.java
+++ Projects/ESGApp/app/Module.java
@@ -10,10 +10,6 @@ import services.Counter;
  * different types. This Guice module is created when the Play
  * application starts.
  *
- * Play will automatically use any class called `Module` that is in
- * the root package. You can create modules in other locations by
- * adding `play.modules.enabled` settings to the `application.conf`
- * configuration file.
  */
 public class Module extends AbstractModule {
 
